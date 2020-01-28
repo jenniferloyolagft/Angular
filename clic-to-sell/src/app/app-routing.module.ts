76fb7f711b52bad1d71976f62d149d9b5158cf5f
Jennifer <jennifer.loyola@gft.com>
2020-01-28 13:06:05 -0600

    servicio agregado

--- clic-to-sell/src/app/app-routing.module.ts
+++ clic-to-sell/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BreedsComponent } from './breeds/breeds.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'breeds', component: BreedsComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
